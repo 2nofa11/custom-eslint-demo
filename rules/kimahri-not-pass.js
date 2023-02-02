30059b609dc636a028c86f0edd27cdc87d86db53
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-02 22:37:46 +0900

    https://qiita.com/kik4/items/ef30d5e0e24dabb81463#%E3%83%AB%E3%83%BC%E3%83%AB%E3%82%92%E5%8B%95%E3%81%8B%E3%81%99 はうまくいく

--- rules/kimahri-not-pass.js
+++ rules/kimahri-not-pass.js
@@ -7,8 +7,10 @@ module.exports = {
       Literal: function (node) {
         if (
           typeof node.value == "string" &&
+          // eslint-disable-next-line rulesdir/kimahri-not-pass
           node.value.indexOf("キマリ") !== -1
         ) {
+          // eslint-disable-next-line rulesdir/kimahri-not-pass
           context.report({ node: node, message: "キマリは通さない" });
         }
       },
