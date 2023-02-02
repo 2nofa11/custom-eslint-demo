77a4ae03df6dd25ea5f27959ab8800beafddf95f
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-02 21:23:03 +0900

    metaの追加（熟読はしていない）

--- src/no-process-node-env.ts
+++ src/no-process-node-env.ts
@@ -1,8 +1,28 @@
-import { Role, Rule } from "eslint";
-
+import { Rule } from "eslint";
+import {
+  MemberExpression,
+  Expression,
+  Super,
+  Identifier,
+  PrivateIdentifier,
+} from "estree";
 const rule: Rule.RuleModule = {
-  meta: {},
-  create(context: Rule.RuleContext) {},
+  meta: {
+    type: "problem",
+    hasSuggestions: true,
+    docs: {
+      description: "許可されていない",
+      suggestion: true,
+    },
+    messages: {
+      unexpectedProcessEnvNodeEnv:
+        "process.env.NODE_ENVは許可されていません。myNodeEnv() を利用してください。",
+      replaceToNodeEnv: "myNodeEnv() に置き換える。",
+    },
+  },
+  create(context: Rule.RuleContext) {
+    return {};
+  },
 };
 
 module.exports = rule;
